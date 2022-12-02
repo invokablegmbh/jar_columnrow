54532d3f5f319f8cdcc26bf83677bf562cfe7e87
Julian Lichte <julian.lichte@jar.media>
2022-12-02 13:16:42 +0000

    Backend styling and icon fix

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'jar-column-row-content-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:jar_columnrow/Resources/Public/Icons/ColumnRow.svg',
+    ],
+];
