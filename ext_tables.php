2dee364e166db481068e35a760b8253db9e19f6f
Container 12409 cc-12409@jcdn.de <script@jcdn.de>
2022-12-02 13:53:09 +0100

    Saved by SUBGIT-Save on Container 1589-marc

---
+++ ext_tables.php
@@ -0,0 +1,10 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+
+$GLOBALS['TBE_STYLES']['skins']['j77_template'] = [
+    'name' => 'jar_columnrow',
+    'stylesheetDirectories' => [
+        'css' => 'EXT:jar_columnrow/Resources/Public/css/'
+    ]
+];
