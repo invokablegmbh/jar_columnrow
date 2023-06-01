dc62943e3e79a50d8e91e61620c2005233b5a82a
Julian Lichte <julian.lichte@jar.media>
2023-06-01 15:43:49 +0000

    Race Conditions while Bootstrapping

--- Classes/ViewHelpers/TsconfigViewHelper.php
+++ Classes/ViewHelpers/TsconfigViewHelper.php
@@ -3,8 +3,6 @@
 namespace Jar\Columnrow\ViewHelpers;
 
 use Jar\Utilities\Utilities\BackendUtility;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
-use TYPO3Fluid\Fluid\Core\Parser\Source;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
@@ -47,12 +45,30 @@ class TsconfigViewHelper extends AbstractViewHelper
         RenderingContextInterface $renderingContext
     ) {
         $options[] = ['LLL:EXT:jar_columnrow/Resources/Private/Language/locallang_be.xlf:transparent','default'];
-        $pageTs = BackendUtility::getCurrentPageTS();
-        if(isset($pageTs['jar']['bgcolors'])) {
-            foreach($pageTs['jar']['bgcolors'] as $label => $value) {
-                $options[] = [$label, $value];
-            }
+
+        $contextVariables = $renderingContext->getVariableProvider()->getAll();
+        if(isset($contextVariables['record'])) {
+            $record = $contextVariables['record'];
+        } else {
+            $record = [];
         }
+
+        // checko for PID, if no PID is present, system is in bootstrapping mode and we can't get the page TS
+        // Otherwise bad things will happen (no new flux elements in content wizard and so on)
+        // Because the page TS is internal cached at the moment of the first access and we would access it to early
+        
+        if(isset($record['pid'])) {
+            $pageTs = BackendUtility::getCurrentPageTS();       
+
+            if (isset($pageTs['jar']['bgcolors'])) {
+                foreach ($pageTs['jar']['bgcolors'] as $label => $value) {
+                    $options[] = [$label, $value];
+                }
+            } 
+        }
+
+
+        
         $options[] = ['LLL:EXT:jar_columnrow/Resources/Private/Language/locallang_be.xlf:user_customized','user'];
         
         return $options;
