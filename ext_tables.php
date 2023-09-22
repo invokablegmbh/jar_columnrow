448a4ef68af13081065d70a56f7ceef45dafc50c
Julian Lichte <julian.lichte@jar.media>
2023-09-22 22:05:31 +0000

    Richtige Column Tabelle gesetzt und bei übersetzten Elementen die Felder ausgeschaltet

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,12 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 $GLOBALS['TBE_STYLES']['skins']['jar_columnrow'] = [
     'name' => 'jar_columnrow',
     'stylesheetDirectories' => [
         'css' => 'EXT:jar_columnrow/Resources/Public/Css/Backend/'
     ]
 ];
+
+ExtensionManagementUtility::allowTableOnStandardPages('tx_jarcolumnrow_columns');
