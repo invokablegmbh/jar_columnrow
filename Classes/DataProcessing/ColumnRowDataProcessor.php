4672b80585d047bbd4e23d1f1ae7f4679c59a5a7
Container 18577 cc-18577@jcdn.de <script@jcdn.de>
2024-01-19 18:14:12 +0100

    Saved by SUBGIT-Save on Container 1623-b13container

--- Classes/DataProcessing/ColumnRowDataProcessor.php
+++ Classes/DataProcessing/ColumnRowDataProcessor.php
@@ -1,7 +1,9 @@
 <?php
 namespace Jar\Columnrow\DataProcessing;
 
+use B13\Container\DataProcessing\ContainerProcessor;
 use Jar\Columnrow\Utilities\ColumnRowUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
@@ -20,7 +22,26 @@ class ColumnRowDataProcessor implements DataProcessorInterface
     public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)
     {
         $processedData = ColumnRowUtility::getFrontendAttributesByPopulatedRow($processedData) + $processedData;
-        debug($processedData);die;
+        $containerProcessor = GeneralUtility::makeInstance(ContainerProcessor::class);
+        $processedData = $containerProcessor->process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
+        $processedData = $this->mapContentInColumns($processedData);
+        return $processedData;
+    }
+
+    protected function mapContentInColumns($processedData) {
+        $columns = $processedData['columns'];
+        foreach($processedData as $key => $value) {
+            $split = explode('_', $key);
+            if(str_contains($key, 'children_')) {
+                foreach($columns as $colKey => $col) {
+                    $strCount = strlen($col['uid']);
+                    if(substr($key, -$strCount) == $col['uid']) {
+                        $processedData['columns'][$colKey]['colPos'] = $split[1];
+                    }
+                }
+            }
+        }
+
         return $processedData;
     }
 }
