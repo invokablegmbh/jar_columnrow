3215a77416dee05423ec34254f5d61442cfe097e
Maurice Möllmer <maurice.moellmer@jar.media>
2024-02-02 13:10:55 +0000

    Update ColumnRowDataProcessor.php

--- Classes/DataProcessing/ColumnRowDataProcessor.php
+++ Classes/DataProcessing/ColumnRowDataProcessor.php
@@ -25,6 +25,50 @@ class ColumnRowDataProcessor implements DataProcessorInterface
         $containerProcessor = GeneralUtility::makeInstance(ContainerProcessor::class);
         $processedData = $containerProcessor->process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
         $processedData = $this->mapContentInColumns($processedData);
+        
+        foreach($processedData['columns'] as $k => $column) {
+            $finalColumnClass = $finalOrderClass = $finalOffsetClass = '';
+            if($column['extended']) {
+                if($column['col_xs'] != '12' && $column['col_xs'] != '0' && ($column['col_xs'] != $column['col_sm'])) {
+                    $finalColumnClass .= ' col-xs-' . $column['col_xs'];
+                }
+                if($column['col_sm'] != '12' && $column['col_sm'] != '0' && ($column['col_sm'] != $column['col_md'])) {
+                    $finalColumnClass .= ' col-sm-' . $column['col_sm'];
+                }
+                if($column['col_md'] != '12' && $column['col_md'] != '0' && ($column['col_md'] != $column['col_lg'])) {
+                    $finalColumnClass .= ' col-md-' . $column['col_md'];
+                }
+
+                if($column['order_xs'] != $column['order_sm']) {
+                    $finalOrderClass .= ' order-xs-' . $column['order_xs'];
+                }
+                if($column['order_sm'] != $column['order_md']) {
+                    $finalOrderClass .= ' order-sm-' . $column['order_sm'];
+                }
+                if($column['order_md'] != $column['order_lg']) {
+                    $finalOrderClass .= ' order-md-' . $column['order_md'];
+                }
+                $finalOrderClass .= ' order-lg-' . $column['order_lg'];
+
+                if($column['offset_xs'] != '12' && $column['offset_xs'] != '0' && ($column['offset_xs'] != $column['offset_sm'])) {
+                    $finalOffsetClass .= ' offset-xs-' . $column['offset_xs'];
+                }
+                if($column['offset_sm'] != '12' && $column['offset_sm'] != '0' && ($column['offset_sm'] != $column['offset_md'])) {
+                    $finalOffsetClass .= ' offset-sm-' . $column['offset_sm'];
+                }
+                if($column['offset_md'] != '12' && $column['offset_md'] != '0' && ($column['offset_md'] != $column['offset_lg'])) {
+                    $finalOffsetClass .= ' offset-md-' . $column['offset_md'];
+                }
+                if($column['offset_lg'] != '12' && $column['offset_lg'] != '0') {
+                    $finalOffsetClass .= ' offset-lg-' . $column['offset_lg'];
+                }
+            }
+            $finalColumnClass .= ' col-lg-' . $column['col_lg'];
+
+            $processedData['columns'][$k]['cssClass'] = $finalColumnClass;
+            $processedData['columns'][$k]['orderClass'] = $finalOrderClass;
+            $processedData['columns'][$k]['offsetClass'] = $finalOffsetClass;
+        }
         return $processedData;
     }
 
